53760dd38c61f0f9f1a9b5249b7453b39cf7ef60
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-06-12 00:11:01 +0530

    commit

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -2,8 +2,11 @@ import Pill from "../components/pill";
 
 export default function Test() {
     return (
-        <div className="flex justify-center items-center h-screen container mx-auto w-5xl border-2 border-red-500">
+        <section className="container mx-auto">
+
+        <div className="flex justify-center items-center h-screen border-2 border-red-500">
             <Pill/>
         </div>
+        </section>
     )
 }
