3397dffed7b61054353da24f1ed52daea4f4cc4d
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-06-13 23:27:31 +0530

    commit

--- src/app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import ContestCard from "../components/contestCard";
-
-export default function Test() {
-    return (
-        <div className="w-full h-screen flex justify-center items-center">
-            
-        </div>
-    )
-}
