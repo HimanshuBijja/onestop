1af26a2b48ed42d0bcbb9134c7269f972bad30a1
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-06-13 23:41:19 +0530

    commit

--- src/app/components/AllContests.tsx
+++ src/app/components/AllContests.tsx
@@ -4,47 +4,55 @@ import ContestCard from "./contestCard";
 interface RenderContestType {
     startTime: number;
     title: string;
-    source : string;
-    link : string;
+    source: string;
+    link: string;
 }
 
 export default async function AllContests() {
-    const response = await axios.get(
-        "https://https://onestop-code.vercel.app/api/contest?source=all&day=all&month=all&sortBy=startTime&sortOrder=asc"
-    );
-    const upcomingContests = response.data.upcomingContests;
+    try {
+        const baseUrl =
+            process.env.NODE_ENV === "production"
+                ? `https://${process.env.VERCEL_URL}`
+                : "http://localhost:3000";
 
+        const response = await axios.get(
+            `${baseUrl}/api/contest?source=all&day=all&month=all&sortBy=startTime&sortOrder=asc`
+        );
+        const upcomingContests = response.data.upcomingContests;
 
-  
-
-
-    return (
-        <section className="relative py-24 px-4">
-            <div className="flex flex-col gap-7 justify-center">
-                {upcomingContests.map((x: RenderContestType, index:any) => (
-                    <RenderContest
-                        source={x.source}
-                        title={x.title}
-                        startTime={Number(x.startTime)}
-                        link={x.link}
-                        key={index}
-                    />
-                ))}
-            </div>
-        </section>
-    );
+        return (
+            <section className="relative py-24 px-4">
+                <div className="flex flex-col gap-7 justify-center">
+                    {upcomingContests.map(
+                        (x: RenderContestType, index: any) => (
+                            <RenderContest
+                                source={x.source}
+                                title={x.title}
+                                startTime={Number(x.startTime)}
+                                link={x.link}
+                                key={index}
+                            />
+                        )
+                    )}
+                </div>
+            </section>
+        );
+    } catch (error) {
+        console.error(error);
+        return <div>Error fetching contests</div>;
+    }
 }
 
-function RenderContest({source, startTime, title, link }: RenderContestType) {
+function RenderContest({ source, startTime, title, link }: RenderContestType) {
     const time = new Date(startTime * 1000);
 
     let hours = time.getHours().toString();
     let minutes = time.getMinutes().toString();
     const seconds = "0" + time.getSeconds();
-    if(Number(hours) < 10){
+    if (Number(hours) < 10) {
         hours = "0" + hours.toString();
     }
-    if(Number(minutes) < 10){
+    if (Number(minutes) < 10) {
         minutes = "0" + minutes.toString();
     }
     const formattedTime = hours + ":" + minutes;
@@ -71,12 +79,13 @@ function RenderContest({source, startTime, title, link }: RenderContestType) {
 
     return (
         <div className="">
-           
-
-            <ContestCard  source={source} label={title} time={formattedTime} date={formattedDate} url={link} />
-
-
+            <ContestCard
+                source={source}
+                label={title}
+                time={formattedTime}
+                date={formattedDate}
+                url={link}
+            />
         </div>
     );
 }
-
